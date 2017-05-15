4e19e81e62e8f3fb278681bf35e405e3c17797a6
Matko Sorić <matko.soric@serengetitech.com>
2017-05-15 10:15:08 +0200

    return example

---
+++ src/edu/matkosoric/methods/returning.java
@@ -0,0 +1,22 @@
+package edu.matkosoric.methods;
+
+/**
+ * Created by matko on 15.5.2017..
+ */
+public class returning {
+
+    public static void main(String[] args) {
+
+
+
+//        int a = method1();      //does not compile, the compiler 'sees' method1 signature with the void return type
+//        System.out.println(a);
+    }
+
+    public static void method1 (){
+//        return 3;       //does not compile, value of return statement (3) does not match the return declaration
+    }
+
+}
+
+
