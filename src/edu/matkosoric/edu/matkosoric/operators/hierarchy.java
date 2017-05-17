27b07690d862a145943d523ab5d558d10a3c2b1b
Matko Sorić <matko.soric@serengetitech.com>
2017-05-17 10:44:06 +0200

    operators precedence & main method correction

---
+++ src/edu/matkosoric/edu/matkosoric/operators/hierarchy.java
@@ -0,0 +1,32 @@
+package edu.matkosoric.edu.matkosoric.operators;
+
+/**
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by Matko Soric.
+ */
+
+public class hierarchy {
+
+    public static void main(String[] args) {
+
+        // java operators have different levels of execution priority.
+        // they form a hierarchy, which simply means that some operations have a priority over other.
+        // that priority can be changed with the use of parentheses ()
+
+
+//        ()        Parentheses
+//        []        Array subscript
+//        ·         Member selection
+
+//        ++        Unary post-increment
+//        --        Unary post-decrement
+
+
+
+        //following statements are valid without parentheses
+
+
+    }
+
+}
