64b7104b6cebed1ab929509a24cfbe9ab06fb060
Matko Sorić <matko.soric@serengetitech.com>
2017-05-18 10:53:50 +0200

    or operator

--- src/edu/matkosoric/edu/matkosoric/operators/hierarchy.java
+++ src/edu/matkosoric/edu/matkosoric/operators/hierarchy.java
@@ -14,17 +14,69 @@ public class hierarchy {
         // they form a hierarchy, which simply means that some operations have a priority over other.
         // that priority can be changed with the use of parentheses ()
 
+/**
+      ()                  Parentheses
+      []                  Array subscript
+      ·                   Member selection
 
-//        ()        Parentheses
-//        []        Array subscript
-//        ·         Member selection
+      ++                  Unary post-increment
+      --                  Unary post-decrement
 
-//        ++        Unary post-increment
-//        --        Unary post-decrement
+      ++                  Unary pre-increment
+      --                  Unary pre-decrement
+      +                   Unary plus
+      -                   Unary minus
+      !                   Unary logical negation
+      ~                   Unary bitwise complement
+      (type)              Unary type cast
 
+      *                   Multiplication
+      /                   Division
+      %                   Modulus
 
+      +                   Addition
+      -                   Subtraction
 
-        //following statements are valid without parentheses
+      <<                  Bitwise left shift
+      >>                  Bitwise right shift with sign extension
+      >>>                 Bitwise right shift with zero extension
+
+      <                   Relational less than
+      <=                  Relational less than or equal
+      >                   Relational greater than
+      >=                  Relational greater than or equal
+      instanceof          Type comparison (objects only)
+
+      ==                  Relational is equal to
+      !=                  Relational is not equal to
+
+      &                   Bitwise AND
+
+      ^                   Bitwise exclusive OR
+
+      |                   Bitwise inclusive OR
+
+      &&                  Logical AND
+
+      ||                  Logical OR
+
+      ? :                 Ternary conditional
+
+      =                   Assignment
+      +=                  Addition assignment
+      -=                  Subtraction assignment
+      *=                  Multiplication assignment
+      /=                  Division assignment
+      %=                  Modulus assignment
+
+ */
+
+        // following statements are valid without parentheses
+        //
+
+        int i = 5 % + 4 - 3 * 2 + 4 - 8 -1 + 3 / 4 % 4 + 1 / 2;
+        boolean a = 65 <= 27 ? 53 != 88 || true ^ !!!false & !true && true : !true ^ 6-2*8+3/1 == 82 / 19;
+
 
 
     }
