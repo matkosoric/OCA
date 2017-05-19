28a591384c3f7d09cae5594b750ca07e9af68ea3
Matko Sorić <matko.soric@serengetitech.com>
2017-05-19 09:27:45 +0200

    super constructor

--- src/edu/matkosoric/edu/matkosoric/operators/hierarchy.java
+++ src/edu/matkosoric/edu/matkosoric/operators/hierarchy.java
@@ -74,7 +74,8 @@ public class hierarchy {
         // following statements are valid without parentheses
         //
 
-        int i = 5 % + 4 - 3 * 2 + 4 - 8 -1 + 3 / 4 % 4 + 1 / 2;
+        int i = 5 % + 4 - 3 * 2 + 4 - 8 - 1 + 3 / 4 % 4 + 1 / 2;
+        boolean k = 234 <= 271;
         boolean a = 65 <= 27 ? 53 != 88 || true ^ !!!false & !true && true : !true ^ 6-2*8+3/1 == 82 / 19;
 
 
