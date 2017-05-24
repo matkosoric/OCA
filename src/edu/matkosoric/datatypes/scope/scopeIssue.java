927224ee2aa51c8e5e80eb9e9bc92b435ee48f19
Matko Sorić <matko.soric@serengetitech.com>
2017-05-24 10:11:40 +0200

    variable initalisation within IF condition

--- src/edu/matkosoric/datatypes/scope/scopeIssue.java
+++ src/edu/matkosoric/datatypes/scope/scopeIssue.java
@@ -1,8 +1,11 @@
 package edu.matkosoric.datatypes.scope;
 
 /**
- * Created by matko on 24.5.2017..
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by Matko Soric.
  */
+
 public class scopeIssue {
 
     public static int number = 777;       // class-wide scope
