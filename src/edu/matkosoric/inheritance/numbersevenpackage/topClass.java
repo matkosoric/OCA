e2109f6286c069850812101b165edb33e73894a4
Matko Sorić <matko.soric@serengetitech.com>
2017-05-31 10:09:45 +0200

    double finally block

--- src/edu/matkosoric/inheritance/numbersevenpackage/topClass.java
+++ src/edu/matkosoric/inheritance/numbersevenpackage/topClass.java
@@ -1,8 +1,11 @@
 package edu.matkosoric.inheritance.numbersevenpackage;
 
 /**
- * Created by matko on 31.5.2017..
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by Matko Soric.
  */
+
 public class topClass {
     // we need this topClass, since top class can not be static.
 
