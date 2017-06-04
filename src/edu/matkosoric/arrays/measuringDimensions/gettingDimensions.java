87ce002cd704e756e240abd09f24b9a2f36524fe
matkosoric <soric.matko@gmail.com>
2017-06-04 21:35:14 +0200

    getting array dimensions

---
+++ src/edu/matkosoric/arrays/measuringDimensions/gettingDimensions.java
@@ -0,0 +1,22 @@
+package edu.matkosoric.arrays.measuringDimensions;
+
+/**
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by Matko Soric.
+ */
+public class gettingDimensions {
+
+    public static void main(String[] args) {
+
+        // first dimension of an array is in the length field.
+        int firstDimension = 8;
+        String [][][][][] stringArray = new String [firstDimension][13][6][2][44];
+        System.out.println("First dimension: " + stringArray.length);
+        System.out.println("Second dimension: " + stringArray[0].length);
+        System.out.println("Third dimension: " + stringArray[0][0].length);
+        System.out.println("Fourth dimension: " + stringArray[0][0][0].length);
+        System.out.println("Fifth dimension: " + stringArray[0][0][0][0].length);
+
+    }
+}
