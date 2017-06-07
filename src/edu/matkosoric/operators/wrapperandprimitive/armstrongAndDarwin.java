87a7363d52df49e49edb09a04886fbf1605dc225
Matko Sorić <matko.soric@serengetitech.com>
2017-06-07 14:51:55 +0200

    comparing wrapper and primitive

---
+++ src/edu/matkosoric/operators/wrapperandprimitive/armstrongAndDarwin.java
@@ -0,0 +1,32 @@
+package edu.matkosoric.operators.wrapperandprimitive;
+
+/**
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by Matko Soric.
+ */
+
+public class armstrongAndDarwin {
+
+    public static void main(String[] args) {
+
+        short moonLandingPrimitive= 1969;
+        Long moonLandingWrapper = 1969L;
+
+        System.out.println(moonLandingWrapper == moonLandingPrimitive);         // true
+        // because wrapper is unboxed
+
+        System.out.println(moonLandingWrapper.equals(moonLandingPrimitive));    // false
+        // because primitive is autoboxed from short to Integer wrapper,
+        // and comparing different classes (wrappers in this case) returns false
+
+
+        int theOriginOfSpeciesPrimitive = 1859;
+        Integer theOriginOfSpeciesWrapper = 1859;
+
+        System.out.println(theOriginOfSpeciesWrapper == theOriginOfSpeciesPrimitive);       // true
+        System.out.println(theOriginOfSpeciesWrapper.equals(theOriginOfSpeciesPrimitive));
+        // true, because primitive is autoboxed to integer wrapper that can be compared to other integer wrapper
+
+    }
+}
