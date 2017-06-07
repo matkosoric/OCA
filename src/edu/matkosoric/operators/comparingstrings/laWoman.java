005faed14bba7c8651c3f8eba01774cbabc3df1c
Matko Sorić <matko.soric@serengetitech.com>
2017-06-07 08:38:16 +0200

    string equality with L.A. woman examples

--- src/edu/matkosoric/operators/comparingstrings/laWoman.java
+++ src/edu/matkosoric/operators/comparingstrings/laWoman.java
@@ -6,7 +6,7 @@ package edu.matkosoric.operators.comparingstrings;
  * Created by Matko Soric.
  */
 
-public class stringequality {
+public class laWoman {
 
     public static void main(String[] args) {
 
@@ -15,19 +15,21 @@ public class stringequality {
         String motel2 = new String("Motel");
         StringBuilder money1 = new StringBuilder("Money");
         StringBuilder money2 = new StringBuilder("Money");
+        String murder1 = "Murder";
+        StringBuilder murder2 = new StringBuilder("Murder");
 
-
-
+        // comparing string with string
         System.out.println(motel1.equals(motel2));
-        System.out.println(motel1 == motel2);       // outputs false, since == operator checks
+        System.out.println((motel1 == motel2) + "\n");       // outputs false, since == operator checks for the object equality
 
+        // comparing stringBuilder with stringBuilder
         System.out.println(money1.equals(money2));
-        System.out.println(money1 == money2);
+        System.out.println((money1 == money2) + "\n");
 
         // comparing String and StringBuilder
-        String murder = "Murder";
-        System.out.println(murder.equals(money1));
-//        System.out.println(murder == money1);       // does not compile, operatoy == can not be used to compare string and stringBuilder
+        System.out.println(murder1.equals(murder2) + "\n");
+//        System.out.println(murder1 == murder2);         // does not compile
+//                                                        // operator == can not be used to compare string and stringBuilder
 
         System.out.println("Madness".equals("Madness"));
         System.out.println("Madness" == "Madness");
