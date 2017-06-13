8cdfaf4fe8b5e6566ee9790076917b892b8bfa50
Matko Sorić <matko.soric@serengetitech.com>
2017-06-13 16:32:21 +0200

    modulus with negative operands

---
+++ src/edu/matkosoric/basics/modulusNegativeByNegative.java
@@ -0,0 +1,21 @@
+package edu.matkosoric.basics;
+
+/**
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by Matko Soric.
+ */
+
+public class modulusNegativeByNegative {
+
+    public static void main(String[] args) {
+
+        // modulus operator returns negative result when first operand is negative
+
+        System.out.println("11 divided by 5 is: " + 11/5 + ", and the remainder is: " + 11 % 5);
+        System.out.println("11 divided by -5 is: " + 11/-5 + ", and the remainder is: " + 11 % -5);
+        System.out.println("-11 divided by 5 is: " + -11/5 + ", and the remainder is: " + -11 % 5);
+        System.out.println("-11 divided by -5 is: " + -11/-5 + ", and the remainder is: " + -11 % -5);
+
+    }
+}
