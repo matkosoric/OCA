ff6c97ee2e4a114fd0067011e412513158ba451c
Matko Sorić <matko.soric@serengetitech.com>
2017-06-13 12:48:22 +0200

    multiline comments within declaration

---
+++ src/edu/matkosoric/basics/commentInDeclaration.java
@@ -0,0 +1,43 @@
+package edu/*
+
+
+
+
+*/.matkosoric.basics;
+
+/**
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by Matko Soric.
+ */
+
+public class commentInDeclaration {
+
+    // multiline comments can be inserted anywhere within a valid code without breaking it
+
+    public static void main(String[] args) {
+
+        String batmanName = "Bruce Wayne";
+        String  /*
+
+
+                                                // weird but valid
+
+
+         */batmanAlias = "Batman";
+
+        flyBatmanFly();
+
+    }
+
+    private static void  /*
+
+
+                                                // weird but valid
+
+
+
+         */flyBatmanFly() {
+    }
+
+}
