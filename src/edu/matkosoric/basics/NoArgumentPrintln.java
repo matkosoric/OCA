aa742a1ce0cdc36dd3059e78e2a8c44eb6743bca
Matko Sorić <matko.soric@serengetitech.com>
2017-06-19 09:38:40 +0200

    no argument println

---
+++ src/edu/matkosoric/basics/NoArgumentPrintln.java
@@ -0,0 +1,19 @@
+package edu.matkosoric.basics;
+
+/**
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by Matko Soric.
+ */
+
+public class NoArgumentPrintln {
+
+    public static void main(String[] args) {
+
+        // System.out.println() works with various arguments, including a no-argument call
+        System.out.println("String");
+        System.out.println(1234);
+        System.out.println();       // compiles just fine, but does not produce any output
+
+    }
+}
---
+++ src/edu/matkosoric/basics/SingleLineInstantiation.java
@@ -0,0 +1,38 @@
+package edu.matkosoric.basics;
+
+/**
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by Matko Soric.
+ */
+
+public class SingleLineInstantiation {
+
+    public static void main(String[] args) {
+
+        // this is valid single-line declaration and instantiation for multiple objects
+        String detective1, detective2, detective3, detective4 = new String();
+        PsychoKiller hannibal, toothFairy = new PsychoKiller();
+        Object movie1, movie2, movie3 = new Object();
+
+        // similar statements do not work with primitive types, since there are no constructors in the Class class.
+//    short year1, year2, year3 = new short();        // does not compile
+//    int n1, n2, n3, n4, n5 = new int();             // does not compile
+//    Double d1, d2, d3 = new Double();               // does not compile
+        int i1, i2, i3;             // valid statement
+        Double h1, h2, h3;          // valid statement
+
+        // this is single-line declaration and initialisation of a String type, that also works with primitives
+        String town1, town2, town3, town4 = "any town";
+//    String county1 = county2 = county3;           // does not compile
+        String village1, village2, village3 = village1 = village2 = "any village";          // weird but valid
+        int population1, population2, population3 = population2 = population1 = 200_000;    // weird but valid
+
+    }
+}
+
+
+class PsychoKiller {
+    String name;
+    int age;
+}
