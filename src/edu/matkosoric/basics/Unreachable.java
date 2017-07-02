2a1366864a6965c4dd59d72e187b4cd2c26ae143
matkosoric <soric.matko@gmail.com>
2017-07-02 13:47:15 +0200

    sunday 2.7 commit

---
+++ src/edu/matkosoric/basics/Unreachable.java
@@ -0,0 +1,57 @@
+package edu.matkosoric.basics;
+
+/*
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by Matko Soric.
+ */
+
+public class Unreachable {
+
+    // unreachable part of the for loop will make a code uncompilable
+
+    public static void main(String[] args) {
+
+        // first, valid loop
+        for (int i = 0; i < 10; i++) {
+
+            if (i == 5) System.out.print("HELLO ");
+            else System.out.print("First loop ");
+
+            System.out.print(i);
+
+            System.out.println(" THIS IS REACHABLE !!!");
+
+        }
+
+
+        // second, valid loop
+        for (int i = 0; i < 10; i++) {
+
+            if (i == 5) break;
+            else System.out.print("Second loop ");
+
+            System.out.print(i);
+
+            System.out.println(" THIS IS REACHABLE !!!");
+
+        }
+
+
+        // third, invalid loop
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("Third loop");
+
+            if (i == 5) break;
+            else continue;
+
+            // because of the if condition, this part of code will never be executed,
+            // since loop will either exit or initiate next iteration after the IF condition
+//            System.out.println(i);                          // does not compile
+//            System.out.println("UNREACHABLE !!!");          // does not compile
+
+        }
+    }
+
+}
---
+++ src/edu/matkosoric/exceptions/redundant/catch_/RedundantCatching.java
@@ -0,0 +1,39 @@
+package edu.matkosoric.exceptions.redundant.catch_;
+
+/*
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by Matko Soric.
+ */
+
+public class RedundantCatching {
+
+    // runtime exceptions do not have to be enclosed with a try-catch block,
+    // or declared in a method signature, but enclosing them or declaring them
+    // will not affect the compilation
+
+    public static void main(String[] args) {
+
+        someMethod1();
+        someMethod2();
+        someMethod3();
+
+    }
+
+    private static void someMethod1() {
+        throw new StringIndexOutOfBoundsException();
+    }
+
+    private static void someMethod2() throws StringIndexOutOfBoundsException{
+        throw new StringIndexOutOfBoundsException();
+    }
+
+    private static void someMethod3() {
+        try {
+            throw new StringIndexOutOfBoundsException();
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Catched!");
+        }
+    }
+
+}
