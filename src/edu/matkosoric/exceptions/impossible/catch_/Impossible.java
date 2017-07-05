b6f589a89eff47033def3a052dcf34cbbe4bc5e1
Matko Sorić <matko.soric@serengetitech.com>
2017-07-05 16:21:57 +0200

    impossible catch

---
+++ src/edu/matkosoric/exceptions/impossible/catch_/Impossible.java
@@ -0,0 +1,26 @@
+package edu.matkosoric.exceptions.impossible.catch_;
+
+/*
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by Matko Soric.
+ */
+
+import java.io.IOException;
+
+public class Impossible {
+
+    // if there is no condition that produces anticipated exception,
+    // the catch part will disable compilation
+
+    public static void main(String[] args) {
+
+//        try {
+//            System.out.println("completely normal code execution");
+//        } catch (IOException e) {                   // does not compile
+//            System.out.println("unreachable");
+//        }
+
+    }
+
+}
