76141cbbd397aa9f1a25d513631df8f0f3904a4d
Matko Sorić <matko.soric@serengetitech.com>
2017-07-11 11:28:25 +0200

    false infinite loop update

--- src/edu/matkosoric/flowcontrol/false_/infinite/loop/FalseInfiniteLoop.java
+++ src/edu/matkosoric/flowcontrol/false_/infinite/loop/FalseInfiniteLoop.java
@@ -14,15 +14,24 @@ public class FalseInfiniteLoop {
 
         // this only looks like an infinite loop,
         // while in fact it has a definite number of iterations,
-        // depending on the counter variable type
+        // depending on the variable type
         while (counter-- < 10) {
             continue;
         }
+        System.out.println("Final value of the counter variable: " + counter + "\n");
 
-        // subtracting from a minimum part of the spectrum
-        // produces value on the maximum part of the spectrum
-        System.out.println(counter);
+        // subtracting one from a minimum value will
+        // produce a maximum value on the other part of the spectrum
+        System.out.println(Integer.MIN_VALUE - 1 + " equals " + Integer.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE - 1 + " equals " + Long.MAX_VALUE);
 
+        // types smaller than Integer are implicitly casted to Integer
+        System.out.println(Short.MIN_VALUE - 1 + " is not equal " + Short.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE - 1 + " is not equal " + Byte.MAX_VALUE);
+
+        // decimal point types also behave weird
+        System.out.println(Double.MIN_VALUE - 1 + " is not equal " + Double.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE - 1 + " is not equal " + Float.MAX_VALUE);
     }
 
 }
