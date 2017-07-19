e3b669d35f58631f41d9d7b9bbacb76e1864df7d
Matko Sorić <matko.soric@serengetitech.com>
2017-07-19 16:48:52 +0200

    string + something = string

---
+++ src/edu/matkosoric/strings/implicit/casting/Ezekiel.java
@@ -0,0 +1,27 @@
+package edu.matkosoric.strings.implicit.casting;
+
+/*
+ * Code examples for Oracle Certified Associate (OCA) Exam
+ * Java 8 SE, 2017.
+ * Created by Matko Soric.
+ */
+
+public class Ezekiel {
+
+    public static void main(String[] args) {
+
+        // operators used with strings will implicitly cast other operands to string and treat them that way
+        String book = "Ezekiel";
+        int chapter = 25;
+        int verse = 17;
+        System.out.println(book + " " + chapter + verse);
+
+        // however, if done before operations with string type, other data types will behave regularly
+        System.out.println(book + " " + (chapter + verse));
+
+        // and more straightforward example
+        System.out.println(6 + 6 + 6 + " execution undisturbed by string type");
+        System.out.println("execution disturbed by string type: " + 6 + 6 + 6);
+
+    }
+}
